dccf8ce431ca7b8d21baefc9a4fc2bdbabc80641
Eugene <yeo.eugene.oey@gmail.com>
2011-07-23 18:31:16 +0800

    clean up code

--- FYP-Android/src/com/rctech/museum/DashboardActivity.java
+++ FYP-Android/src/com/rctech/museum/DashboardActivity.java
@@ -1,30 +1,15 @@
 package com.rctech.museum;
 
-import java.util.Scanner;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.provider.MediaStore.Video;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class DashboardActivity extends Activity {
-
-	// Define Request code to be used in startActivityForResult
-	private static final int SCAN_CODE = 1;
-	private static final int LOAD_CODE = 2;
-	private static final int SEARCH_CODE = 3;
-	private static final int DEMO_CODE = 4;
-	private static final int VISITED_CODE = 5;
 
 	JSONObject json;
 
