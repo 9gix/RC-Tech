15b3e9ecd52b182f07e36dfa0ca6d566f44b3495
Eugene <yeo.eugene.oey@gmail.com>
2011-07-26 18:43:07 +0800

    Integrate About Page by Felicia

--- FYP-Android/src/com/rctech/museum/AboutActivity.java
+++ FYP-Android/src/com/rctech/museum/AboutActivity.java
@@ -2,12 +2,22 @@ package com.rctech.museum;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
+        setContentView(R.layout.about);
+        TextView aboutUs;
+        aboutUs = (TextView)findViewById(R.id.aboutUs);
+        aboutUs.setText("This application is created by " +
+        		"Eugene, Felicia and Tran from Republic Polytechnic. " +
+        		"\n\nMuseum Partner is used as a guide, helping " +
+        		"to  enhance the user experience during your museum visits."
+        );
+        
+        aboutUs.setTextSize(18);
 	}
 }
