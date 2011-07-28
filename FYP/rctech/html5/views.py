386a56e2e9e88207a42e26a76c0e8ce75b782f20
Eugene <yeo.eugene.oey@gmail.com>
2011-07-29 02:46:22 +0800

    create rc-tech welcome page for mobile user

--- FYP/rctech/html5/views.py
+++ FYP/rctech/html5/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, render_to_response
 from django.views.generic.simple import direct_to_template
 
 def index(request):
-    return direct_to_template(request, 'html5/index.html',{})
+    return direct_to_template(request, 'html5/welcome.html',{})
